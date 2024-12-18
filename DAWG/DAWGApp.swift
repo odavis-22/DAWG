ba0b03d93ddf47c2590fa9e52f7457a678be4e2a
Owen Davis <owendavis1115@gmail.com>
2024-12-18 12:29:38 -0500

    Initial Commit

---
+++ DAWG/DAWGApp.swift
@@ -0,0 +1,17 @@
+//
+//  DAWGApp.swift
+//  DAWG
+//
+//  Created by Owen Davis (student LM) on 12/18/24.
+//
+
+import SwiftUI
+
+@main
+struct DAWGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
